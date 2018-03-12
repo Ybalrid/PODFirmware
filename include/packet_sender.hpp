16ec01e8494e2fff410ce5515a710e1d85b03c9e
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2018-03-12 15:40:52 +0100

    Fix packet sender

--- include/packet_sender.hpp
+++ include/packet_sender.hpp
@@ -14,26 +14,26 @@ class packet_sender
     struct sockaddr_in si_server;
     int s; //socket kernel file descriptor
     struct POD_WalkVector walkVector;
-    char* server_address
+    std::string server_address;
 
     public:
         void setServerAddress(const std::string& addr)
         {
-            server_address = addr.c_str();
-            inet_aton(server_address, &si_server.sin_addr);
+            server_address = addr;
+            inet_aton(server_address.c_str(), &si_server.sin_addr);
         }
 
         packet_sender()
         {
             s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-            memset((char*)&si_sever, 0, sizeof(si_server));
+            memset((char*)&si_server, 0, sizeof(si_server));
             si_server.sin_family = AF_INET;
-            si_sever.sin_port = htons(POD_PROTOCOL_PORT);
+            si_server.sin_port = htons(POD_PROTOCOL_PORT);
         }
 
         ~packet_sender()
         {
-            close(socket);
+            close(s);
         }
 
 
@@ -48,7 +48,7 @@ class packet_sender
                     (POD_Byte*)&walkVector,
                     sizeof walkVector,
                     0,
-                    (struct sockaddr*)&si_other,
-                    sizeof si_other);
+                    (struct sockaddr*)&si_server,
+                    sizeof si_server);
         }
 };
